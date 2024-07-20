6dff5a7736dd39d1e393837c412f262da6bc169a
Samira <tokhirovasamira.17@gmail.com>
2024-07-20 16:09:00 +0500

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-import { getData } from "./libs/http";
+import { getData } from "./libs/http"
 
+let swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  }
+})
 
 getData('movie/now_playing')
-  .then(res => console.log(res))
-  .catch(error => console.error(error))
-
-let test = ''
+.then(res => console.log(res))
+.catch(error => console.error(error))

4f0d1b4cfab40cd268ee2cf7c74626e5311366ad
Samira <tokhirovasamira.17@gmail.com>
2024-07-20 15:48:38 +0500

    test

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ import { getData } from "./libs/http";
 
 getData('movie/now_playing')
   .then(res => console.log(res))
-  .catch(error => console.error(error)) 
+  .catch(error => console.error(error))
+
+let test = ''

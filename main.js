6c74c14f9ff4afdb335a49efe419010d006bddd8
Mushtariy <mushtariy.nosirova2005@gmail.com>
2024-07-25 12:24:18 +0500

    test

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ let btnAllMovies = document.querySelector('.all-movies')
 getData('movie/now_playing')
   .then(res => {
     reload(res.data.results.slice(0,8), 'movies', NowPlaying)
-    btnAllMovies.onclick = () => {
-    reload(res.data.results, 'movies', NowPlaying)
-  }
     console.log(res);
   })
   .catch(error => console.error(error)) 
 
-  
+   btnAllMovies.onclick = () => {
+    window.location.replace('/pages/NowPlayingMovies/')
+  }
 

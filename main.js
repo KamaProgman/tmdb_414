0960114ad94a025a027f4a6c47fec1de7b58161e
Samira <tokhirovasamira.17@gmail.com>
2024-07-27 14:59:11 +0500

    test

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-import { addPostersToSwiper,  NowPlaying } from "./components/posters";
+import { ActorsRating } from "./components/actorsRating";
+import { NowPlaying } from "./components/nowPlaying";
+import { PopularActors } from "./components/popularActors";
+import { addPostersToSwiper } from "./components/posters";
 import { getData } from "./libs/http";
 import { reload } from "./libs/utils";
 
-let btnAllMovies = document.querySelector('.all-movies')
 
 getData('movie/now_playing')
-    .then(res => {
-        console.log(res.data);
-        addPostersToSwiper(res.data.results);    
-        reload(res.data.results.slice(0,8), 'movies', NowPlaying)
-    })
-    .catch(error => console.error(error));
+.then(res => {
+    addPostersToSwiper(res.data.results);    
+    reload(res.data.results.slice(0,8), 'movies', NowPlaying)
+})
+.catch(error => console.error(error));
 
+getData('movie/top_rated')
+.then(res => {
+    reload(res.data.results.slice(0, 4), 'popular-movies', NowPlaying)
+})
+.catch(error => console.error(error))
 
-   btnAllMovies.onclick = () => {
-    window.location.replace('/pages/NowPlayingMovies/')
-  }
+getData('person/popular')
+.then(res => {
+    reload(res.data.results.slice(0,3), 'actors-box', ActorsRating)
+})
+.catch(error => console.error(error))
 
+getData('person/popular')
+.then(res => {
+    console.log(res.data);
+    reload(res.data.results.slice(0,2), 'popular-actors', PopularActors)
+  })
+  .catch(error => console.error(error))
+  
+
+let btnAllMovies = document.querySelector('.all-movies')
+btnAllMovies.onclick = () => {
+ window.location.replace('/pages/NowPlayingMovies/')
+}
